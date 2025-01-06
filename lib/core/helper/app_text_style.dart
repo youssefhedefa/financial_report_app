b7781a6930793288a912dbabfabd7b52792a7cdc
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2025-01-06 12:08:19 +0200

    Create app text style class.

---
+++ lib/core/helper/app_text_style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle{
+  static const TextStyle font32BlackSemiBold = TextStyle(
+    fontSize: 32,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle font26BlackRegular = TextStyle(
+    fontSize: 26,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+}
