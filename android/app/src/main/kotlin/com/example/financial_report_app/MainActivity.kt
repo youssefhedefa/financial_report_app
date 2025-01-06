7d01d3b9646f4c03ca3b6d3be6b697f0ee185090
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2025-01-06 11:16:57 +0200

    First commit.

---
+++ android/app/src/main/kotlin/com/example/financial_report_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.financial_report_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:financial_report_app/financial_report_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
