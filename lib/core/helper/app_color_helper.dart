28ad88d989653e3819ebd40ff5ffefa6ccab5ea6
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2025-01-06 12:08:59 +0200

    Add green color to app color helper.

--- lib/core/helper/app_color_helper.dart
+++ lib/core/helper/app_color_helper.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 abstract class AppColorHelper{
   static const Color yellowColor = Color(0xFFEEFF8A);
+  static const Color softCyanBlueColor = Color(0xFFE5EEEF);
+  static const Color greenColor = Color(0xFFB0FEB6);
 }
