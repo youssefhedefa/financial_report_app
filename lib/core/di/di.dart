8cc298c77a8f7bc3281ec109e611b5b8765be839
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2025-01-06 13:30:47 +0200

    Setup dependence injection by get it.

---
+++ lib/core/di/di.dart
@@ -0,0 +1,19 @@
+import 'package:financial_report_app/core/data/data_source/orders_data_source.dart';
+import 'package:financial_report_app/core/data/repos_imple/metric_repo_imple.dart';
+import 'package:financial_report_app/features/metrics/domain/repo/metric_repo.dart';
+import 'package:financial_report_app/features/metrics/presentation/manager/get_matric_data_cubit/get_matric_data_cubit.dart';
+import 'package:get_it/get_it.dart';
+
+var getIt = GetIt.instance;
+
+void setupDi() {
+  getIt.registerLazySingleton<OrdersDataSource>(() => OrdersDataSource());
+  getIt.registerLazySingleton<MetricRepo>(() => MetricRepoImple(
+    ordersDataSource: getIt<OrdersDataSource>(),
+  ));
+  getIt.registerFactory(() => GetMetricDataCubit(
+    getIt<MetricRepo>(),
+  ));
+
+
+}
