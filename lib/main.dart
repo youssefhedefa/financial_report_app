0c9154b2efc68bf8155ee85c648e25262844f4fc
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2025-01-06 13:31:58 +0200

    Add setup function to main function.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:financial_report_app/core/di/di.dart';
 import 'package:financial_report_app/financial_report_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  setupDi();
   runApp(const MyApp());
 }
 
